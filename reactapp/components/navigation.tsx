d11ab44ef2b1f29e3d9f67afe6bfee2484800701
normal-Fag <72517164+normal-Fag@users.noreply.github.com>
2022-01-03 00:40:03 +0800

    Исправления в navigation

--- reactapp/components/navigation.tsx
+++ reactapp/components/navigation.tsx
@@ -1,49 +1,55 @@
 import { useState } from "react"
+import { start } from "repl";
+import { EventEmitter } from "stream";
 
 export function Navigation(props: {isItFooterNav: boolean}) {
-    // const body = document.querySelector('body');
-    // const header = document.getElementById('.header');
-    // const headerBurger = document.getElementById('.header__burger');
-    // const headerMenuList = document.getElementById('.menu')
 
-    const linksOnClick = (body: HTMLElement, header: HTMLElement, 
-        headerBurgerBtn: HTMLElement, menuList: HTMLElement, pointerEvent: EventTarget) => {
-        console.log(pointerEvent);
-        // pointerEvent.preventDefault();
-    
-        // const blockValue = pointerEvent.dispatchEvent.
-        // document.getElementById('.' + pointerEvent.currentTarget.getAttribute('href')).getBoundingClientRect().top + pageYOffset - 110;
-    
+    const [navigation, setNavigation] = useState([
+        {
+            href: 'welcome-section',
+            title: 'Главная'
+        },
+        {
+            href: 'sightseens',
+            title: 'Достопримечательности'
+        },
+        {
+            href: 'tours',
+            title: 'Туры'
+        },
+        {
+            href: 'contacts',
+            title: 'Контакты'
+        },
+    ])
+
+    const scrollToBlock = (e: MouseEvent) => {
+        e.preventDefault();
+        // const yOffset = -72;
+        let block = document.querySelector('.' + e.currentTarget.getAttribute('href'));
+        // const y = block?.getBoundingClientRect().top + yOffset + window.pageYOffset;
+
         // window.scrollTo({
-        //     top: blockValue ,
+        //     top: y,
         //     behavior: 'smooth'
-        // });
-        // body.classList.remove('lock');
-        // header.classList.remove('active');
-        // headerBurgerBtn.classList.remove('active');
-        // menuList.classList.remove('active');
+        // })
+
+        block && block.scrollIntoView({behavior: "smooth", block: "start"});
     }
 
     return(
         <nav className={ props.isItFooterNav ? 'bottom-menu' : 'menu menu-active'}>
             <h3 className="bottom-menu__title">Меню</h3>
             <ul className={props.isItFooterNav ? 'bottom-menu__list' : 'menu__list'}>
-                <li className={props.isItFooterNav ? 'bottom-menu__item' : 'menu__item'}>
-                    <a 
-                        href="welcome-section" 
-                        className={props.isItFooterNav ? 'bottom-menu__link' : 'menu__link'}
-                        // onClick={(event) => linksOnClick(body, header, headerBurger, headerMenuList, event)}
-                        >Главная</a>
-                </li>
-                <li className={props.isItFooterNav ? 'bottom-menu__item' : 'menu__item'}>
-                    <a href="sightseens" className={props.isItFooterNav ? 'bottom-menu__link' : 'menu__link'}>Достопримечательности</a>
-                </li>
-                <li className={props.isItFooterNav ? 'bottom-menu__item' : 'menu__item'}>
-                    <a href="tours" className={props.isItFooterNav ? 'bottom-menu__link' : 'menu__link'}>Туры</a>
-                </li>
-                <li className={props.isItFooterNav ? 'bottom-menu__item' : 'menu__item'}>
-                    <a href="contacts" className={props.isItFooterNav ? 'bottom-menu__link' : 'menu__link'}>Контакты</a>
-                </li>
+                {navigation.map((navlink, index) => 
+                    <li className={props.isItFooterNav ? 'bottom-menu__item' : 'menu__item'}>
+                        <a 
+                            href={navlink.href} 
+                            className={props.isItFooterNav ? 'bottom-menu__link' : 'menu__link'}
+                            onClick={e => scrollToBlock(e)}
+                            >{navlink.title}</a> 
+                    </li>
+                )}
             </ul>
         </nav>
     )
